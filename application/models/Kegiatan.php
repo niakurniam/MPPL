87ca119fe5f49175a631b88e44a08f4a054bcb89
Mustika Kurnia M <nia.kurniam@yahoo.co.id>
2017-05-02 15:52:50 +0700

    update model Kegiatan

--- application/models/Kegiatan.php
+++ application/models/Kegiatan.php
@@ -7,6 +7,7 @@ class Kegiatan extends CI_Model{
 
 	function event_today(){
 		$curr_date = date("Y-m-d");
+		//$curr_time = date("H:i", time());
 		//ambil data event dari tabel DEvent
 		$listevent = $this->db->query("select nama_event, tanggal_mulai, jam_mulai, tanggal_selesai, jam_selesai, tempat from kegiatan where tanggal_mulai = '$curr_date'");
 		return $listevent->result();
