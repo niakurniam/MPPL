5c7db1fac2e5473e89312f3bf9e89bacd44f8c63
Mustika Kurnia M <nia.kurniam@yahoo.co.id>
2017-05-03 16:14:27 +0700

    update sql terbaru

--- application/models/Kepengurusan.php
+++ application/models/Kepengurusan.php
@@ -7,7 +7,7 @@ class Kepengurusan extends CI_Model{
 
 	function tampil_kepengurusan(){
 		//ambil data event dari tabel DEvent
-		$listkepengurusan = $this->db->query("select nama_kepengurusan, tahun_mulai, tahun_berakhir from kepengurusan");
+		$listkepengurusan = $this->db->query("select nama_kepengurusan, tahun_mulai, tahun_berakhir from kepengurusan order by tahun_mulai asc");
 		return $listkepengurusan->result();
 	}
 
