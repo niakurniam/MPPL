5c9d083fb1f8880e75612f4d3fc5ef948391e708
Nur Maulidiah El Fajr <Nur Maulidiah El Fajr>
2017-05-14 21:09:14 +0700

    Update dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -247,8 +247,8 @@
                     <div class="footer-block buttons"> <iframe class="footer-github-btn" src="https://ghbtns.com/github-btn.html?user=modularcode&repo=modular-admin-html&type=star&count=true" frameborder="0" scrolling="0" width="140px" height="20px"></iframe> </div>
                     <div class="footer-block author">
                         <ul>
-                            <li> created by <a href="https://github.com/modularcode">ModularCode</a> </li>
-                            <li> <a href="https://github.com/modularcode/modular-admin-html#get-in-touch">get in touch</a> </li>
+                            <li>  <a href="">Kelompok C-05</a> </li>
+                            <li> <a href="">Manajemen Proyek Perangkat Lunak</a> </li>
                         </ul>
                     </div>
                 </footer>
